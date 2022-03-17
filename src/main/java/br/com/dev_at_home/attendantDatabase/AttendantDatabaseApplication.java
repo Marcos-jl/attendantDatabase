01550b5905b4044580ac005438375490657e1137
Marcos Cunha <marcos.cunha@ffit.com.br>
2022-03-17 12:56:40 -0300

    Create Spring

---
+++ src/main/java/br/com/dev_at_home/attendantDatabase/AttendantDatabaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev_at_home.attendantDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendantDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendantDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dev_at_home/attendantDatabase/AttendantDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev_at_home.attendantDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendantDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
